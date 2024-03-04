df40b9893bb4aa63e67b8f50477baba7a5230242
Andrew Nasef <androamazing@gmail.com>
2024-03-04 00:40:34 +0000

    fix: centering the main element in page layout

--- app.vue
+++ app.vue
@@ -7,7 +7,7 @@ import "@/assets/main.scss";
     class="flex min-h-screen flex-col selection:bg-primary-500 selection:text-white-100"
   >
     <Header />
-    <div class="m-auto w-full max-w-screen-xl">
+    <div class="mx-auto w-full max-w-screen-xl flex-grow">
       <div class="flex h-full flex-grow flex-col gap-16 p-8 py-12 sm:p-16">
         <NuxtPage />
       </div>
