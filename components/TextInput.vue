f847122ce1b7fb8689aad18aa64633d19e73cc7c
Andrew Nasef <androamazing@gmail.com>
2024-03-12 19:28:17 +0000

    feat: author settings

---
+++ components/TextInput.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+interface Props {
+  name: string;
+  label: string;
+  value: "";
+  placeholder: string;
+  size?: "normal" | "textarea";
+  type?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  name: "",
+  label: "",
+  value: "",
+  placeholder: "",
+  size: "normal",
+  type: "text",
+});
+</script>
+
+<template>
+  <div class="grid gap-4">
+    <label :for="name" class="text-bg font-semibold text-white-75">{{
+      label
+    }}</label>
+
+    <textarea
+      :name="name"
+      class="h-11 w-full resize-none rounded border-2 border-white-75 bg-transparent p-2 outline-none focus:border-white-100 md:max-w-[400px]"
+      :type="type"
+      :class="{ 'min-h-24 resize-y': size === 'textarea' }"
+      size="text"
+      :value="value"
+      :placeholder="placeholder"
+    />
+  </div>
+</template>
+
+<style></style>
---
+++ pages/mysettings.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h2 class="text-3xl font-bold">Your Settings</h2>
+  <form class="grid gap-8">
+    <h3 class="text-2xl font-bold">Public Profile</h3>
+    <img
+      class="w-32 rounded-full"
+      src="https://media.vanityfair.com/photos/5edcf34989654e1d12a48bf0/1:1/w_1600%2Cc_limit/jk-rowling.jpg"
+      alt=""
+    />
+    <TextInput label="Name" />
+    <TextInput label="Username" />
+    <TextInput size="textarea" label="Description" />
+    <div class="flex gap-3">
+      <Button variant="secondary"> Cancel </Button>
+      <Button variant="primary"> Save </Button>
+    </div>
+  </form>
+</template>
+
+<style></style>
