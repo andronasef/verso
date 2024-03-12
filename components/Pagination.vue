490a90df7692912a5edde9e739ad2e9a16901661
Andrew Nasef <androamazing@gmail.com>
2024-03-12 18:38:44 +0000

    chore: extract pagination component

---
+++ components/Pagination.vue
@@ -0,0 +1,28 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+  <div
+    class="flex items-stretch justify-center gap-2 [&>*]:rounded [&>*]:border-white-25"
+  >
+    <Button variant="secondary"
+      ><Icon
+        name="streamline:interface-arrows-button-left-double-arrow-arrows-double-left"
+    /></Button>
+
+    <Button variant="secondary">1</Button>
+    <Button variant="primary" :action="undefined">2</Button>
+    <Button variant="secondary">3</Button>
+    <Button variant="secondary">4</Button>
+    <Button variant="secondary"
+      ><Icon
+        name="streamline:interface-arrows-button-right-duoble-arrow-arrows-double-right"
+        size="14"
+    /></Button>
+  </div>
+</template>
+
+<style>
+
+</style>
--- pages/author/[[username]].vue
+++ pages/author/[[username]].vue
@@ -32,24 +32,7 @@ const { author, posts } = {
     <PostCard v-for="i in 6" />
   </div>
   <!-- Pagination -->
-  <div
-    class="flex items-stretch justify-center gap-2 [&>*]:rounded [&>*]:border-white-25"
-  >
-    <Button variant="secondary"
-      ><Icon
-        name="streamline:interface-arrows-button-left-double-arrow-arrows-double-left"
-    /></Button>
-
-    <Button variant="secondary">1</Button>
-    <Button variant="primary" :action="undefined">2</Button>
-    <Button variant="secondary">3</Button>
-    <Button variant="secondary">4</Button>
-    <Button variant="secondary"
-      ><Icon
-        name="streamline:interface-arrows-button-right-duoble-arrow-arrows-double-right"
-        size="14"
-    /></Button>
-  </div>
+  <Pagination />
 </template>
 
 <style></style>
